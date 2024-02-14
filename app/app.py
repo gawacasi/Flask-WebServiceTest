28d2d69a213f7de018a4378af8130d7d8d5d5dea
gawacasi <gwcs@cin.ufpe.br>
2024-02-14 13:55:27 -0300

    TESTE

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Index"
+
+@app.route("/teste")
+def teste():
+    return "kkkrisos"
+
+if __name__ == '__main__':
+    app.run()
+
+
+
