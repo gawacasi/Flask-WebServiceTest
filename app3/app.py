46484697bb2b03218c23e69377dd9b3a0b9ccd46
gawacasi <gwcs@cin.ufpe.br>
2024-02-14 14:28:47 -0300

    TESTE

--- app3/app.py
+++ app3/app.py
@@ -11,7 +11,7 @@ def hello(nome=""):
 @app.route('/blog/<int:postID>')
 def blog(postID= -1):
     if postID >= 0:
-        return "blog info {}".format(postID)
+        return "blog infos {}".format(postID)
     else:
         return "Blog todo"
 
